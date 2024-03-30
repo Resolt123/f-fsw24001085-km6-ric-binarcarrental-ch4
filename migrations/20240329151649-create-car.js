cf9d632cb87c4d964ac042081d066919937a4f66
Richard <richardaurelio42@gmail.com>
2024-03-30 21:20:02 +0700

    first commit challenge 4 binar

---
+++ migrations/20240329151649-create-car.js
@@ -0,0 +1,60 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("cars", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      plate: {
+        type: Sequelize.STRING,
+      },
+      manufacture: {
+        type: Sequelize.STRING,
+      },
+      model: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.TEXT,
+      },
+      rentPerDay: {
+        type: Sequelize.INTEGER,
+      },
+      capacity: {
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      alevailabAt: {
+        type: Sequelize.DATE,
+      },
+      transmission: {
+        type: Sequelize.STRING,
+      },
+      available: {
+        type: Sequelize.BOOLEAN,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      year: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("cars");
+  },
+};
---
+++ route/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const car = require("./car");
+const option = require("./option")
+const spec = require("./spec")
+
+router.use("/car", car);
+
+router.use("/option", option);
+
+router.use("/spec", spec);
+
+module.exports = router;
